13504355028d728d894f149e11614e23728f27b6
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-03-01 15:15:46 +0600

    details added

--- js/rummage-phone.js
+++ js/rummage-phone.js
@@ -36,4 +36,27 @@ const loadPhoneDetail = phoneId =>{
 }
 const showPhoneDetail = phone =>{
     console.log(phone);
+    const phoneDetails = document.getElementById('phone-details');
+    const div = document.createElement('div');
+    div.classList.add("card");
+    div.innerHTML = `
+   
+  <img src="${phone.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${phone.brand}</h5>
+    <h6 class="card-title">${phone.brand}</h6>
+    <p class="card-text"> <span class='fw-bold'>Release Date:</span> ${phone.releaseDate}</p>
+    <p class="card-text"><span class='fw-bold'>Features of the Phone:</span> ${phone.mainFeatures.chipSet}</p>
+    <p class="card-text"><span class='fw-bold'>Display Size:</span> ${phone.mainFeatures.displaySize}</p>
+    <p class="card-text"><span class='fw-bold'>Memory Details:</span> ${phone.mainFeatures.memory}</p>
+    <p class="card-text"><span class='fw-bold'>Storage:</span> ${phone.mainFeatures.storage}</p>
+
+
+    <p class="card-text"><span class='fw-bold'>Available sensors:</span> ${phone.mainFeatures.sensors[0]},  ${phone.mainFeatures.sensors[1]},  ${phone.mainFeatures.sensors[2]},  ${phone.mainFeatures.sensors[3]},  ${phone.mainFeatures.sensors[4]},  ${phone.mainFeatures.sensors[5]}</p>
+    
+  
+</div>
+    
+    `;
+    phoneDetails.appendChild(div);
 }
