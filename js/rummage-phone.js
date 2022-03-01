b61cde02044a3bfd28629be01e3babd21ea09bdf
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-03-01 13:26:31 +0600

    first search result added

--- js/rummage-phone.js
+++ js/rummage-phone.js
@@ -6,5 +6,24 @@ const searchPhone = () => {
     // console.log(url)
     fetch (url)
     .then(res => res.json())
-    .then(data => console.log(data.data[0].brand))
+    .then(data => displaySearchResult(data.data));
+}
+const displaySearchResult = data =>{
+    const searchResult = document.getElementById('search-result');
+    data.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML =
+        `
+        <div class="card h-100">
+        <img src= "${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${phone.brand}</h5>
+            <p class="card-text">${phone.phone_name}</p>
+    </div>
+  </div>
+        `;
+        searchResult.appendChild(div)
+    });
 }
